378cb1e574cb86f12a929fa07406af689dc4927c
Châu <nhchau89@gmail.com>
2015-05-04 15:27:30 +0700

    Update Extendsion Shop By Brand - Add Brand to Top menu

---
+++ app/code/local/BlueCom/BrandExample/Model/Observer.php
@@ -0,0 +1,14 @@
+<?php
+    class BlueCom_BrandExample_Model_Observer {
+        public function addToTopMenu(Varien_Event_Observer $observer) {
+            $menu = $observer->getMenu();
+            $tree = $menu->getTree();
+            $node = new Varien_Data_Tree_Node(array(
+                'name' => 'BRANDS',
+                'id' => 'brands',
+                'url' => Mage::getUrl().'/brand/',
+            ), 'id', $tree, $menu);
+
+            $menu->addChild($node);
+        }
+    }
