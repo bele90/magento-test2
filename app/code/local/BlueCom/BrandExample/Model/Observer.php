b75bf9d9f5868f58a886fae7a43dabbad5d7ab6e
Châu <nhchau89@gmail.com>
2015-05-06 16:35:35 +0700

    update SHOP BY BRAND

--- app/code/local/BlueCom/BrandExample/Model/Observer.php
+++ app/code/local/BlueCom/BrandExample/Model/Observer.php
@@ -10,5 +10,21 @@
             ), 'id', $tree, $menu);
 
             $menu->addChild($node);
+
+            $collection = Mage::getModel('bluecom_branddirectory/brand')->getCollection()
+                ->addFieldToFilter('visibility', BlueCom_BrandDirectory_Model_Brand::VISIBILITY_DIRECTORY)
+                ->setOrder('entity_id', 'ASC');
+
+            foreach($collection as $brand) {
+                $tree = $node->getTree();
+                $data = array(
+                    'name' => $brand->getName(),
+                    'id' => 'brand-node-'.$brand->getId(),
+                    'url' => Mage::getUrl().'brand/index/view/url/'.$brand->getUrlKey(),
+                );
+
+                $subNode = new Varien_Data_Tree_Node($data, 'id', $tree, $node);
+                $node->addChild($subNode);
+            }
         }
     }
